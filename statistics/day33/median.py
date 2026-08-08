440cba9a08526d16be2afaae634b060e0b3eeaab
Solapuram Siva Kumar Reddy <solapuramsivakumar703@gmail.com>
2026-08-08 23:12:36 +0530

    day 33 median

---
+++ statistics/day33/median.py
@@ -0,0 +1,7 @@
+marks = [72, 85, 91, 67, 88]
+
+sorted_list = sorted(marks)
+middle_index = len(sorted_list) // 2
+middle_element = sorted_list[middle_index]
+print(sorted_list)
+print(middle_element)
---
+++ statistics/day33/median_numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+marks = [72, 85, 91, 67, 88]
+
+median = np.median(marks)
+
+print(median)
